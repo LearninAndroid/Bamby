c9dd346540ebde05b7276a3c2f934c3109441e8d
mtotodev05 <mtotodev05@gmail.com>
2018-06-22 00:01:10 +0300

    more views

--- app/src/main/java/dev/brian/com/bamby/MainActivity.java
+++ app/src/main/java/dev/brian/com/bamby/MainActivity.java
@@ -1,13 +1,26 @@
 package dev.brian.com.bamby;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Login = (Button)findViewById(R.id.btnLogin);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(MainActivity.this,Home.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        });
+
     }
 }
